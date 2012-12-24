e17bf2e0d2c86830239591d3c007f0d140179584
Sung-En Wang <supersanyo@gmail.com>
2012-12-24 12:38:59 -0800

    Update .gitignore Fix typo on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible
-  has_many :accesses, foregin_key:"user_id", dependent: :destroy
+  has_many :accesses, foreign_key:"user_id", dependent: :destroy
   has_many :ss_classes, through: :ss_instructors, source: :ss_class
   #has_many :ss_classes, through: :ss_class_sessions, source: :ss_class
   has_many :sessions, through: :ss_attendances, source: :ss_class_session
-  has_many :ss_class_sessions, foregin_key:"instructor_id", dependent: :destroy
+  has_many :ss_class_sessions, foreign_key:"instructor_id", dependent: :destroy
 
 end
