2540f18bd1e68cce3d2370ce93745ed42fec0572
Sung-En Wang <supersanyo@gmail.com>
2012-12-28 23:01:37 -0800

    remvoe bible obj

--- app/models/bible.rb
+++
@@ -1,7 +0,0 @@
-class Bible < ActiveRecord::Base
-  attr_accessible :name
-  has_many :ss_classes
-
-  validates :name,
-            :presence => true
-end
--- db/migrate/20121215225354_create_bibles.rb
+++
@@ -1,9 +0,0 @@
-class CreateBibles < ActiveRecord::Migration
-  def change
-    create_table :bibles do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
