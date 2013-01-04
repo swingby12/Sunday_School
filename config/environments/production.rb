1de935aee224a6c6744ae1c697a120d4746b046c
Sung-En Wang <supersanyo@gmail.com>
2013-01-04 00:48:01 -0800

    bug fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ SundaySchool::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( jquery.tokeninput.js token-input.css token-input-mac.css token-input-facebook.css )
+  config.assets.precompile += %w( jquery.tokeninput.js token-input.css token-input-mac.css token-input-facebook.css bootstrap-editable.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
